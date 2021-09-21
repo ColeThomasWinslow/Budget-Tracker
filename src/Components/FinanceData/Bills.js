6b71d92d63845f935e05d8cfbb3f2acbd28fe8bb
Cole Winslow <colethomaswinslow7@gmail.com>
2021-09-20 20:29:44 -0700

    Added inputs

--- src/Components/FinanceData/Bills.js
+++ src/Components/FinanceData/Bills.js
@@ -3,10 +3,15 @@ import React from "react";
 function Bills() {
   return (
     <div>
-      <h4 className="Question">What are your reacquiring monthly bills?</h4>
-      <p className="Label">Utilities</p>
-      <p className="Label">Rent</p>
-      <p className="Label">Insurance</p>
+      <div>
+        <h4 className="Question">What are your reacquiring monthly bills?</h4>
+        <p className="Label">Utilities</p>
+        <input type="number" />
+        <p className="Label">Rent</p>
+        <input type="number" />
+        <p className="Label">Insurance</p>
+        <input type="number" />
+      </div>
       <button className="AddBtn Custom">Add Custom Bill</button>
     </div>
   );
