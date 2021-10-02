36853d94e41411a5b50fbd8d537d4557931a7f1f
Cole Winslow <colethomaswinslow7@gmail.com>
2021-10-01 23:40:48 -0700

    changed server

--- server.js
+++ server.js
@@ -22,11 +22,9 @@ if (process.env.NODE_ENV === "development") {
 app.use("/api/v1/transactions", transactions);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("../client/build"));
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
-  );
+  this.app.use(express.static(path.join(__dirname, "../client/build")));
 }
+
 const PORT = process.env.PORT || 5000;
 app.listen(
   PORT,
